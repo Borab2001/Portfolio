98de4b49d6bb0374473d1647a90df6b56e76d276
Bora <borabalos9@gmail.com>
2023-07-14 06:18:07 +1000

    Smooth scroll with locomotive-scroll

---
+++ portfolio/src/app/page.js
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect } from 'react';
+import styles from './page.module.css';
+
+export default function Home() {
+
+  useEffect(() => {
+    {
+      async () => {
+        const LocomotiveScroll = (await import('locomotive-scroll')).default;
+        const locomotiveScroll = new LocomotiveScroll();
+      }
+    }
+  }, []);
+
+  return (
+    <main className={styles.main}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </main>
+  )
+}

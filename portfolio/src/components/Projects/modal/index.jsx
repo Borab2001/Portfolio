cffa47b437ff980d1632d9e5d9446311aca8c9f3
Bora <borabalos9@gmail.com>
2023-07-14 14:27:35 +1000

    Modal scale animation w/ FramerMotion

--- portfolio/src/components/Projects/modal/index.jsx
+++ portfolio/src/components/Projects/modal/index.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import styles from './style.module.css';
 import Image from "next/image";
 import Link from "next/link";
+import { motion } from "framer-motion";
+
+const scaleAnimation = {
+    initial: {scale: 0, x: "-50%", y: "-50%"},
+    open: {scale: 1, x: "-50%", y: "-50%", transition: {duration: 0.4, ease: [0.76, 0, 0.24, 1]}},
+    closed: {scale: 0, x: "-50%", y: "-50%", transition: {duration: 0.4, ease: [0.32, 0, 0.67, 0]}}
+}
 
 export default function index({modal, projects}) {
 
     const { active, index } = modal;
 
     return (
-        <div className={styles.modalContainer}>
+        <motion.div variants={scaleAnimation} initial={"initial"} animate={active ? "open" : "closed"} className={styles.modalContainer}>
             <div style={{top: index * -100 + "%"}} className={styles.modalSlider}>
                 {
                     projects.map((project, index) => {
@@ -29,6 +36,6 @@ export default function index({modal, projects}) {
                     })
                 }
             </div>
-        </div>
+        </motion.div>
     )
 }

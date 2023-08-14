5956cac8dcd5f5719499bd9ccb350af94f63ddf1
Bora <borabalos9@gmail.com>
2023-08-14 19:00:50 +1000

    Loading screen + Intro spacement

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -1,31 +1,46 @@
 "use client";
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './page.module.css';
 
 import Intro from '../components/Intro/Index';
 import Description from '../components/Description/Index';
+import Preloader from '../components/Preloader/Index';
 import Projects from '../components/Projects/Index';
 import Footer from '../components/Footer/Index';
-
+import { AnimatePresence } from 'framer-motion';
 
 export default function Home() {
 
-  useEffect(() => {
-    (
-      async () => {
-        const LocomotiveScroll = (await import('locomotive-scroll')).default;
-        const locomotiveScroll = new LocomotiveScroll();
-      }
-    )()
-  }, []);
+	const [isLoading, setIsLoading] = useState(true);
 
-  return (
-    <main className={styles.main}>
-      <Intro />
-      <Description />
-      <Projects />
-      <Footer />
-    </main>
-  )
+	useEffect(() => {
+		(
+			async () => {
+				const LocomotiveScroll = (await import('locomotive-scroll')).default;
+				const locomotiveScroll = new LocomotiveScroll();
+
+				setTimeout(() => {
+					setIsLoading(false);
+					document.body.style.cursor = 'default';
+					window.scrollTo(0, 0);
+				}, 2000);
+			}
+		)();
+	}, []);
+
+	return (
+		<main className={styles.main}>
+			<AnimatePresence mode='wait'>
+				{
+					isLoading && <Preloader />
+				}
+			</AnimatePresence>
+
+			<Intro />
+			<Description />
+			<Projects />
+			<Footer />
+		</main>
+	);
 }

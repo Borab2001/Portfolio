c2e3009a5a60cb532cfd7e680d3542eae5a00bb7
Bora <borabalos9@gmail.com>
2023-09-21 23:38:52 +1000

    Clickable modal cursor

--- portfolio/src/components/Projects/modal/Index.jsx
+++ portfolio/src/components/Projects/modal/Index.jsx
@@ -15,48 +15,58 @@ const scaleAnimation = {
 }
 
 export default function Index({modal, projects}) {
-
     const { active, index } = modal;
     const modalContainer = useRef(null);
-    // const cursor = useRef(null);
-    // const cursorLabel = useRef(null);
+    const cursor = useRef(null);
+    const cursorLabel = useRef(null);
 
     useEffect(() => {
         let xMoveContainer = gsap.quickTo(modalContainer.current, "left", {duration: 0.8, ease: "power3"})
         let yMoveContainer = gsap.quickTo(modalContainer.current, "top", {duration: 0.8, ease: "power3"})
 
-        // let xMoveCursor = gsap.quickTo(cursor.current, "left", {duration: 0.5, ease: "power3"})
-        // let yMoveCursor = gsap.quickTo(cursor.current, "top", {duration: 0.5, ease: "power3"})
-
-        // let xMoveCursorLabel = gsap.quickTo(cursorLabel.current, "left", {duration: 0.45, ease: "power3"})
-        // let yMoveCursorLabel = gsap.quickTo(cursorLabel.current, "top", {duration: 0.45, ease: "power3"})
-
+        // Inside the useEffect hook:
 
         window.addEventListener("mousemove", (e) => {
             const { clientX, clientY } = e;
             const containerRect = modalContainer.current.getBoundingClientRect();
-            // const cursorRect = cursor.current.getBoundingClientRect();
-            // const cursorLabelRect = cursorLabel.current.getBoundingClientRect();
+            const cursorRect = cursor.current.getBoundingClientRect();
             
             const xContainer = clientX - containerRect.width / 2;
             const yContainer = clientY - containerRect.height / 2;
-            
-            // const xCursor = clientX - cursorRect.width / 2;
-            // const yCursor = clientY - cursorRect.height / 2;
-            
-            // const xCursorLabel = clientX - cursorLabelRect.width / 2;
-            // const yCursorLabel = clientY - cursorLabelRect.height / 2;
-            
-            // gsap.to(container.current, {left: x, top: y, duration: 0.8, ease: "power3"});
+
             xMoveContainer(xContainer);
             yMoveContainer(yContainer);
-            // xMoveCursor(xCursor);
-            // yMoveCursor(yCursor);
-            // xMoveCursorLabel(xCursorLabel);
-            // yMoveCursorLabel(yCursorLabel);
+
+            // Check if real cursor overlaps with custom cursor
+            if(clientX > cursorRect.left && clientX < cursorRect.right && clientY > cursorRect.top && clientY < cursorRect.bottom) {
+                // If they overlap, change the real cursor style to pointer
+                document.body.style.cursor = 'pointer';
+            } else {
+                // Otherwise, reset the cursor style
+                document.body.style.cursor = 'auto';
+            }
         });
-    }, [])
-    
+
+        const handleClick = (e) => {
+            const { clientX, clientY } = e;
+            const cursorRect = cursor.current.getBoundingClientRect();
+            
+            // Check if real cursor overlaps with custom cursor
+            if(clientX > cursorRect.left && clientX < cursorRect.right && clientY > cursorRect.top && clientY < cursorRect.bottom){
+                // If they overlap, navigate to the desired URL
+                window.location.href = activeProjectHref;
+            }
+        };
+
+        window.addEventListener("click", handleClick);
+
+        return () => {
+            window.removeEventListener("click", handleClick);
+        }
+
+    }, [projects, modal.index])
+
+    const activeProjectHref = projects[modal.index]?.href;
 
     return (
         <>
@@ -80,9 +90,10 @@ export default function Index({modal, projects}) {
                         })
                     }
                 </div>
+                <motion.a ref={cursor} href={activeProjectHref} variants={scaleAnimation} initial={"initial"} animate={active ? "open" : "closed"} className={styles.cursor}>
+                    <motion.div ref={cursorLabel} variants={scaleAnimation} initial={"initial"} animate={active ? "open" : "closed"} className={styles.cursorLabel}>View</motion.div>
+                </motion.a>
             </motion.div>
-            {/* <motion.a ref={cursor} href={href} variants={scaleAnimation} initial={"initial"} animate={active ? "open" : "closed"} className={styles.cursor}></motion.a>
-            <motion.div ref={cursorLabel} variants={scaleAnimation} initial={"initial"} animate={active ? "open" : "closed"} className={styles.cursorLabel}>View</motion.div> */}
         </>
     )
 }

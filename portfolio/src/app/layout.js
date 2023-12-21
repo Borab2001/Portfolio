d9f0b4e0d74ed88c918a0592a02fea48523618ef
Bora <borabalos9@gmail.com>
2023-12-21 11:36:04 +0100

    Next speed-insights

--- portfolio/src/app/layout.js
+++ portfolio/src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({ children }) {
 			<body className={inter.className}>
 				{children}
 				<Analytics />
+				<SpeedInsights/>
 			</body>
 		</html>
 	);

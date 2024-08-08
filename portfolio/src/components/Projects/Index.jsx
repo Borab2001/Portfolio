254c3b9d1ddc63c35de1def7bcc63f87a22b7722
Bora <borabalos9@gmail.com>
2024-08-08 19:17:04 +0200

    Add aila consulting to projects content

--- portfolio/src/components/Projects/Index.jsx
+++ portfolio/src/components/Projects/Index.jsx
@@ -8,46 +8,53 @@ export default function Index() {
 
     const projects = [
         {
-          title: "Curious",
-          subtitle: "A social knowledge portal powered with ChatGPT built with Next.js, React, TypeScript, TailwindCSS, Firebase & LangChain.",
-          src: "curious.webp",
-          href: "https://curious-gpt.vercel.app/",
-          color: "#4F46E5",
+			title: "Curious",
+			subtitle: "A social knowledge portal powered with ChatGPT built with Next.js, React, TypeScript, TailwindCSS, Firebase & LangChain.",
+			src: "curious.webp",
+			href: "https://curious-gpt.vercel.app/",
+			color: "#4F46E5",
         },
         {
-          title: "Foodify",
-          subtitle: "Full stack multi-functional recipe app built with Next.js, React.js, TailwindCSS, NextAuth, Prisma & MongoDB.",
-          src: "foodify.webp",
-          href: "https://foodifyapp.vercel.app/",
-          color: "#4CD264"
+			title: "Foodify",
+			subtitle: "Full stack multi-functional recipe app built with Next.js, React.js, TailwindCSS, NextAuth, Prisma & MongoDB.",
+			src: "foodify.webp",
+			href: "https://foodifyapp.vercel.app/",
+			color: "#4CD264"
         },
         {
-          title: "Link",
-          subtitle: "Full stack real-time messaging app built with Next.js, React.js, TailwindCSS, NextAuth, Prisma, MongoDB & Pusher.",
-          src: "link.webp",
-          href: "https://link-bb.vercel.app/",
-          color: "#1B171C"
+			title: "Link",
+			subtitle: "Full stack real-time messaging app built with Next.js, React.js, TailwindCSS, NextAuth, Prisma, MongoDB & Pusher.",
+			src: "link.webp",
+			href: "https://link-bb.vercel.app/",
+			color: "#1B171C"
         },
         {
-          title: "Cinephile",
-          subtitle: "Full Stack Movie Streaming app built with Next.js, React.js, TailwindCSS, NextAuth, Prisma & MongoDB.",
-          src: "cinephile.webp",
-          href: "https://cinephileapp.vercel.app/",
-          color: "#390C6F"
+			title: "Cinephile",
+			subtitle: "Full Stack Movie Streaming app built with Next.js, React.js, TailwindCSS, NextAuth, Prisma & MongoDB.",
+			src: "cinephile.webp",
+			href: "https://cinephileapp.vercel.app/",
+			color: "#390C6F"
         },
         {
-          title: "Cypheme",
-          subtitle: "Cypheme Anti-Counterfeit Technologies website which included visual rebranding, administrative automation, code optimization, cross-platform compatibility & numerous other landing pages.",
-          src: "cypheme.webp",
-          href: "https://cypheme.com/",
-          color: "#F0EFF9"
+			title: "Cypheme",
+			subtitle: "Cypheme Anti-Counterfeit Technologies website which included visual rebranding, administrative automation, code optimization, cross-platform compatibility & numerous other landing pages.",
+			src: "cypheme.webp",
+			href: "https://cypheme.com/",
+			color: "#F0EFF9"
         },
         {
-          title: "Groupe BNSB",
-          subtitle: "Website of Groupe BNSB - Design & Sourcing office, currently in build with Next.js, React.js, FramerMotion and LenisScroll (Old version build with HTML, CSS & JS available on groupe-bnsb.com).",
-          src: "groupebnsb.webp",
-          href: "https://groupe-bnsb.vercel.app/",
-          color: "#ECEFF3"
+			title: "Groupe BNSB",
+			subtitle: "Website of Groupe BNSB - Design & Sourcing office, currently in build with Next.js, TypeScript, TailwindCSS FramerMotion and LenisScroll (Old version build with HTML, CSS & JS available on groupe-bnsb.com).",
+			src: "groupebnsb.webp",
+			href: "https://groupe-bnsb.vercel.app/",
+			color: "#ECEFF3"
+        },
+        {
+			title: "Aila Consulting",
+			subtitle: "Website of Aila Consulting, currently in build with Next.js, TypeScript, TailwindCSS and FramerMotion.",
+			src: "ailaconsulting.webp",
+			href: "https://ailaconsulting.vercel.app/",
+			color: "#FFFFFF"
         }
     ]
 

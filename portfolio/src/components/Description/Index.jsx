0d7b1082f4bacf52909ed4b83be1c6960d66bd5e
Bora <borabalos9@gmail.com>
2024-11-06 21:15:22 +0100

    Revert to previous description code

--- portfolio/src/components/Description/Index.jsx
+++ portfolio/src/components/Description/Index.jsx
@@ -6,75 +6,131 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useLayoutEffect, useRef } from "react";
 
 export default function Index() {
-    const sections = [
-        {
-            title: "MS in Computer Science",
-            content: (
-                <>
-                    <span>MS in Computer Science </span>
-                    <a href='https://www.isep.fr/'>@ISEP</a>
-                    <span> 🇫🇷 & </span>
-                    <a href='https://www.sydney.edu.au/'>@SydneyUni</a>
-                    <span> 🇦🇺</span>
-                </>
-            ),
-        },
-        {
-            title: "Intern at",
-            content: (
-                <>
-                    <span>Intern at </span> 
-                    <a href='https://www.capgemini.com/'>Capgemini</a>
-                    <span> ♠ 🇫🇷</span>
-                </>
-            ),
-        },
-        { title: "My Interests", content: "Passionate about creating & designing" },
-        { title: "Sports", content: "Sports (played high-level of football)" },
-        { title: "Music", content: "Making beats on FL Studio" },
-        { title: "My Activities", content: "Food distribution for people in need at Paris City Hall" },
-        { title: "International Students", content: "Creation Pole in an association helping international students" },
-        { title: "Student Jobs", content: "Member of a job service association for students" },
-        { title: "My Work", content: "Like working on ambitious and diverse projects" },
-        { title: "Current Work", content: "Currently working on Maison Anaée’s fully automatized e-commerce web app with admin dashboard" },
-        { title: "Previous Work", content: "Ex-front-end engineer intern at Cypheme Anti-Counterfeit Technologies" },
-        { title: "Carbon Footprint", content: "Worked on a carbon footprint calculator for the startup Tickarbone" },
-        { title: "Projects", content: "Here are some of my projects:" }
-    ];
+    // const phrases = [
+    // `Engineering Student ${<a href='https://www.isep.fr/'>@ISEP 🇫🇷</a>} & ${<a href='https://www.sydney.edu.au/'>@SydneyUni 🇦🇺</a>}`,
+    // "Passionate about front-end & design",
+    // "Working on ambitious and diverse projects"
+    //     "Engineering Student ",
+    //     <a href='https://www.isep.fr/'>@ISEP 🇫🇷</a>,
+    //     " & ",
+    //     <a href='https://www.sydney.edu.au/'>@SydneyUni 🇦🇺</a>,
+    //     "Passionate about front-end & design",
+    //     "Working on ambitious and diverse projects"
+    // ];
 
     return (
         <div className={styles.description}>
-            {sections.map((section, index) => (
-                <AnimatedSection key={index} title={section.title}>
-                    {section.content}
-                </AnimatedSection>
-            ))}
+            <AnimatedText>
+                <h2>
+                    <span>MS in Computer Science </span>
+                    <a href='https://www.isep.fr/'>@ISEP</a>
+                    <span> &#x1F1EB;&#x1F1F7; &amp; </span>
+                    <a href='https://www.sydney.edu.au/'>@SydneyUni</a>
+                    <span> &#x1F1E6;&#x1F1FA;</span>
+                </h2>
+            </AnimatedText>
+            <AnimatedText>
+                <h2>
+                    <span>Intern at </span> 
+                    <a href='https://www.capgemini.com/'>Capgemini</a>
+                    <span> &#x2660; &#x1F1EB;&#x1F1F7;</span>
+                </h2>
+            </AnimatedText>
+            <AnimatedText>
+                <h2>
+                    My Interests
+                </h2>
+            </AnimatedText>
+            <AnimatedText>
+                <p>
+                    Passionate about creating &amp; designing
+                </p>
+            </AnimatedText>
+            <AnimatedText>
+                <p>
+                    Sports (played high-level of football)
+                </p>
+            </AnimatedText>
+            <AnimatedText>
+                <p>
+                    Making beats on FL Studio
+                </p>
+            </AnimatedText>
+            <AnimatedText>
+                <h2>
+                    My Activities
+                </h2>
+            </AnimatedText>
+            <AnimatedText>
+                <p>
+                    Food distribution for people in need at Paris City Hall
+                </p>
+            </AnimatedText>
+            <AnimatedText>
+                <p>
+                    Creation Pole in an association helping international students
+                </p>
+            </AnimatedText>
+            <AnimatedText>
+                <p>
+                    Member of a job service association for students
+                </p>
+            </AnimatedText>
+            <AnimatedText>
+                <h2>
+                    My Work
+                </h2>
+            </AnimatedText>
+            <AnimatedText>
+                <p>
+                    Like working on ambitious and diverse projects
+                </p>
+            </AnimatedText>
+            <AnimatedText>
+                <p>
+                    Currently working on Maison Ana&eacute;&rsquo;s fully automatized e-commerce web app with admin dashboard
+                </p>
+            </AnimatedText>
+            <AnimatedText>
+                <p>
+                    Ex-front-end engineer intern at Cypheme Anti-Counterfeit Technologies
+                </p>
+            </AnimatedText>
+            <AnimatedText>
+                <p>
+                    Worked on a carbon footprint calculator for the startup Tickarbone
+                </p>
+            </AnimatedText>
+            <AnimatedText>
+                <p>
+                    Here are some of my projects:
+                </p>
+            </AnimatedText>
         </div>
-    );
+    )
 }
 
-function AnimatedSection({ title, children }) {
-    const sectionRef = useRef(null);
+function AnimatedText({children}) {
+
+    const text = useRef(null);
 
     useLayoutEffect(() => {
         gsap.registerPlugin(ScrollTrigger);
 
-        gsap.from(sectionRef.current, {
+        gsap.from(text.current, {
             scrollTrigger: {
-                trigger: sectionRef.current,
+                trigger: text.current,
                 start: "0px bottom",
                 end: "bottom+=400px bottom",
                 scrub: true
             },
             left: "-200px",
             opacity: 0
-        });
-    }, []);
+        })
 
+    }, []);
+    
     return (
-        <div ref={sectionRef}>
-            <h2>{title}</h2>
-            <p>{children}</p>
-        </div>
-    );
+        <div ref={text}>{children}</div>
+    )
 }

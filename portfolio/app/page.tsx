05f6b671dee1b06e78759870cac97ebccedb2d17
Bora <borabalos9@gmail.com>
2025-01-27 18:50:42 +0100

    Memoji added to hero with image in webp

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,15 +1,24 @@
 // import { CornerRightDown } from 'lucide-react';
 
+import Image from "next/image";
+
 export default function Home() {
     return (
-		<div className="min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-			<main>
-				<div className="flex flex-col justify-center items-center gap-4">
-					<h1 className="text-3xl sm:text-7xl font-medium  text-black dark:text-white text-center">
+		<div className="flex min-h-screen font-[family-name:var(--font-geist-sans)]">
+			<main className="h-screen w-full flex items-center justify-center p-8 sm:p-20">
+				<div className="flex flex-col justify-center items-center gap-4 md:gap-6">
+					<Image 
+						src="/images/memoji.webp" 
+						alt="Memoji"
+						width={96}
+						height={96}
+						className="rounded-full shadow-sm shadow-black dark:shadow-white ring-1 ring-white dark:ring-white"
+					/>
+					<h1 className="text-4xl sm:text-7xl font-medium  text-black dark:text-white text-center">
 						Bora Balos
 					</h1>
-					<p className="text-lg md:text-2xl leading-relaxed tracking-tight text-muted-foreground max-w-2xl text-center">
-						A French software engineer crafting clean web<br />with a keen eye for design
+					<p className="text-lg md:text-2xl leading-relaxed tracking-tight text-muted-foreground max-w-72 md:max-w-lg text-center">
+						A French software engineer crafting clean web with a keen eye for design
 					</p>
 				
 					<div className="flex flex-row gap-4 pt-4">

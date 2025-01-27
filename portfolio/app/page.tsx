d0b296edc75733ca7b7f7ae3249a97853d4181e6
Bora <borabalos9@gmail.com>
2025-01-27 19:16:27 +0100

    Hero responsive and installed lucide react

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,32 +1,31 @@
-// import { CornerRightDown } from 'lucide-react';
-
+import { ArrowDown } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
     return (
 		<div className="flex min-h-screen font-[family-name:var(--font-geist-sans)]">
 			<main className="h-screen w-full flex items-center justify-center p-8 sm:p-20">
-				<div className="flex flex-col justify-center items-center gap-4 md:gap-6">
+				<div className="flex flex-col justify-center items-center gap-4 sm:gap-6 md:gap-8">
 					<Image 
 						src="/images/memoji.webp" 
 						alt="Memoji"
-						width={96}
-						height={96}
-						className="rounded-full shadow-sm shadow-black dark:shadow-white ring-1 ring-white dark:ring-white"
+						width={128}
+						height={128}
+						className="h-24 w-24 sm:h-32 sm:w-32 rounded-full shadow-sm shadow-black dark:shadow-white ring-1 ring-white dark:ring-white"
 					/>
-					<h1 className="text-4xl sm:text-7xl font-medium  text-black dark:text-white text-center">
+					<h1 className="text-5xl sm:text-7xl md:text-8xl font-medium text-black dark:text-white text-center">
 						Bora Balos
 					</h1>
-					<p className="text-lg md:text-2xl leading-relaxed tracking-tight text-muted-foreground max-w-72 md:max-w-lg text-center">
+					<p className="text-lg sm:text-xl md:text-2xl leading-relaxed tracking-tight text-muted-foreground max-w-96 md:max-w-lg text-center">
 						A French software engineer crafting clean web with a keen eye for design
 					</p>
 				
-					<div className="flex flex-row gap-4 pt-4">
-						<button className="flex flex-row gap-4 px-4 py-2 font-medium bg-black dark:bg-white text-white dark:text-black rounded-full">
+					<div className="flex flex-row gap-4 md:gap-6 pt-4">
+						<button className="flex flex-row items-center justify-center gap-1 pl-4 pr-3 py-2 text-base md:text-lg font-medium bg-black dark:bg-white text-white dark:text-black rounded-full">
 							View my work 
-							{/* <CornerRightDown className="w-4 h-4" /> */}
+							<ArrowDown className="w-5 h-5" />
 						</button>
-						<button className="">
+						<button className="text-base md:text-lg font-medium text-black dark:text-white">
 							My Resume
 						</button>
 					</div>

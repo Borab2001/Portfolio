562e9fa39093768c2281980889b7850390d3d42a
Bora <borabalos9@gmail.com>
2025-01-28 01:28:12 +0100

    feat: Basic styling for card

--- portfolio/components/ui/card.tsx
+++ portfolio/components/ui/card.tsx
@@ -5,8 +5,10 @@ const Card = ({
     link
 }) => {
     return (
-        <div>
-            <p>{title}</p>
+        <div className="p-8 sm:p-20">
+            <div className="w-full max-w-5xl h-auto aspect-video dark:bg-zinc-900 border dark:border-zinc-700 rounded-xl">
+                <h2 className="text-2xl">{title}</h2>
+            </div>
         </div>
     );
 }

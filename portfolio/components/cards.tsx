6a5fed5db2b4b18b4b14c52e836deb21252856b5
Bora <borabalos9@gmail.com>
2025-01-28 12:34:26 +0100

    feat: Calculating scroll progress of cards with use ref

--- portfolio/components/cards.tsx
+++ portfolio/components/cards.tsx
@@ -1,4 +1,8 @@
+import { useEffect, useRef } from "react";
 import Card from "./ui/card";
+
+import { motion, useScroll } from "motion/react";
+
 
 const Cards = () => {
     const projects = [
@@ -16,9 +20,20 @@ const Cards = () => {
             alt: 'BNSB Project image',
             link: '/'
         },
-    ]
+    ];
+
+    const container = useRef(null);
+    const { scrollYProgress } = useScroll({
+        target: container,
+        offset: ['start start', 'end end']
+    });
+
+    useEffect(() => {
+        scrollYProgress.on("change", e => console.log(scrollYProgress));
+    })
+
     return (
-        <div>
+        <div ref={container}>
             {projects.map((project, index) => {
                 return <Card key={index} index={index} {...project} />
             })}

cc09f2f84e6ace1ab1540bf661fe33d0ae7705aa
Bora <borabalos9@gmail.com>
2025-01-28 18:31:08 +0100

    Word by word paragraph with animated opacity on scroll

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,11 +1,16 @@
 import Cards from "@/components/cards";
 import Hero from "@/components/hero";
+import Paragraph from "@/components/paragraph";
 
 export default function Home() {
+
+	const paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam.";
+
     return (
 		<main className="flex flex-col min-h-screen font-[family-name:var(--font-geist-sans)]">
 			<Hero />
 			<Cards />
+			<Paragraph value={paragraph} />
 		</main>
     );
 }

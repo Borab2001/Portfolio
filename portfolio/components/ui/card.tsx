a353b6034191181f038f46dfa8a6e642f397d5ee
Bora <borabalos9@gmail.com>
2025-01-28 01:54:15 +0100

    feat: Styling card with sticky positioning and passing data to style it

--- portfolio/components/ui/card.tsx
+++ portfolio/components/ui/card.tsx
@@ -1,13 +1,36 @@
-const Card = ({
+import Image from "next/image";
+import Link from "next/link";
+
+interface CardProps {
+    title: string;
+    description: string;
+    src: string;
+    link: string;
+}
+
+const Card: React.FC<CardProps> = ({
     title,
     description,
     src,
     link
 }) => {
     return (
-        <div className="p-8 sm:p-20">
-            <div className="w-full max-w-5xl h-auto aspect-video dark:bg-zinc-900 border dark:border-zinc-700 rounded-xl">
-                <h2 className="text-2xl">{title}</h2>
+        <div className="h-screen p-8 sm:p-20 flex items-center justify-center sticky top-0">
+            <div className="relative -top-[10%] w-full max-w-5xl h-auto aspect-video p-4 dark:bg-zinc-900 border dark:border-zinc-700 rounded-xl grid grid-cols-2">
+                <div className="cols-span-1">
+                    <div>
+                        <h2 className="text-2xl">{title}</h2>
+                        <p>{description}</p>
+                    </div>
+                    <Link href={link} target="blank">View Project</Link>
+                </div>
+                <Image 
+                    src={src} 
+                    alt="" 
+                    className="cols-span-1"
+                    width={500}
+                    height={500}    
+                />
             </div>
         </div>
     );

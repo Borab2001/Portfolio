c05b2055b25fd7eef7ec12619c8496257a642fbd
Bora <borabalos9@gmail.com>
2025-01-29 22:05:39 +0100

    feat: Props to pass children and classnames from bento grid to elements

--- portfolio/components/bento-grid.tsx
+++ portfolio/components/bento-grid.tsx
@@ -3,10 +3,10 @@ import BentoElement from "./ui/bento-element";
 const BentoGrid = () => {
     return (
         <div className="p-4 sm:p-8 w-full max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
-                <BentoElement />
-                <BentoElement />
-                <BentoElement />
-                <BentoElement />
+                <BentoElement>Grid 1</BentoElement>
+                <BentoElement>Grid 2</BentoElement>
+                <BentoElement>Grid 3</BentoElement>
+                <BentoElement>Grid 4</BentoElement>
         </div>
     );
 }
--- portfolio/components/ui/bento-element.tsx
+++ portfolio/components/ui/bento-element.tsx
@@ -1,7 +1,15 @@
-const BentoElement = () => {
+interface BentoElementProps {
+    className?: string;
+    children?: React.ReactNode;
+}
+
+const BentoElement: React.FC<BentoElementProps> = ({
+    className,
+    children
+}) => {
     return (
-        <div className="w-full h-[320px] bg-zinc-100 dark:bg-zinc-900 border border-zinc-300 dark:border-zinc-700 rounded-3xl overflow-hidden">
-            Bento
+        <div className={`w-full h-[320px] bg-zinc-100 dark:bg-zinc-900 border border-zinc-300 dark:border-zinc-700 rounded-3xl overflow-hidden ${className}`}>
+            {children}
         </div>
     );
 }

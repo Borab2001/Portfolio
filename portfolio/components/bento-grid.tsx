13c5e6462a45b11ce111b30463eec3f20655aea9
Bora <borabalos9@gmail.com>
2025-01-30 00:48:08 +0100

    style: Bento responsiveness and card color adjustments

--- portfolio/components/bento-grid.tsx
+++ portfolio/components/bento-grid.tsx
@@ -2,11 +2,13 @@ import BentoElement from "./ui/bento-element";
 
 const BentoGrid = () => {
     return (
-        <div className="p-4 sm:p-8 w-full max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-4">
-            <BentoElement className="col-span-1 md:col-span-1">Grid 1</BentoElement>
-            <BentoElement className="col-span-1 md:col-span-2">Grid 2</BentoElement>
-            <BentoElement className="col-span-1 md:col-span-2">Grid 3</BentoElement>
-            <BentoElement className="col-span-1 md:col-span-1">Grid 4</BentoElement>
+        <div className="p-4 sm:p-8 w-full">
+            <div className="w-full max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-4">
+                <BentoElement className="col-span-1 md:col-span-1">Grid 1</BentoElement>
+                <BentoElement className="col-span-1 md:col-span-2">Grid 2</BentoElement>
+                <BentoElement className="col-span-1 md:col-span-2">Grid 3</BentoElement>
+                <BentoElement className="col-span-1 md:col-span-1">Grid 4</BentoElement>
+            </div>
         </div>
     );
 }
--- portfolio/components/ui/card.tsx
+++ portfolio/components/ui/card.tsx
@@ -30,7 +30,7 @@ const Card: React.FC<CardProps> = ({
     return (
         <div className="h-screen p-4 sm:p-8 md:p-20 flex items-center justify-center sticky top-0">
             
-            <motion.div style={{ scale: cardScale, top: `calc(-10% + ${index * 5}vh)` }} className="relative -top-[10%] w-full max-w-5xl h-auto aspect-video p-2 sm:p-4 dark:bg-zinc-900 border dark:border-zinc-700 rounded-xl grid grid-cols-1 lg:grid-cols-2 gap-4">
+            <motion.div style={{ scale: cardScale, top: `calc(-10% + ${index * 5}vh)` }} className="relative -top-[10%] w-full max-w-5xl h-auto aspect-video p-2 sm:p-4 bg-[#F4F4F5] dark:bg-zinc-900 border border-zinc-300 dark:border-zinc-700 rounded-xl grid grid-cols-1 lg:grid-cols-2 gap-4">
                 <div className="cols-span-1 flex flex-col justify-between gap-4 order-2 lg:order-1">
                     <div className="flex flex-col gap-2">
                         <h2 className="text-xl md:text-3xl">{title}</h2>

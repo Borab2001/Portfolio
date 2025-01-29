6b95b740b781f673de79630b2357ef90a1ed01bc
Bora <borabalos9@gmail.com>
2025-01-29 21:45:46 +0100

    feat: Basic empty bento element

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -2,6 +2,7 @@ import Cards from "@/components/cards";
 import Hero from "@/components/hero";
 import Paragraph from "@/components/paragraph";
 
+
 export default function Home() {
 
 	const paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam.";
---
+++ portfolio/components/ui/bento-element.tsx
@@ -0,0 +1,9 @@
+const BentoElement = () => {
+    return (
+        <div className="bg-zinc-100 dark:bg-zinc-950 border border-zinc-300 dark:border-zinc-700 rounded-3xl overflow-hidden">
+            Bento
+        </div>
+    );
+}
+ 
+export default BentoElement;

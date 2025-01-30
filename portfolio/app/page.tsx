8c10e4da44b8b9349ccf7cb95d7b087a138c25c1
Bora <borabalos9@gmail.com>
2025-01-30 17:17:35 +0100

    feat: Tilting card component

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -2,6 +2,7 @@ import BentoGrid from "@/components/bento-grid";
 import Cards from "@/components/cards";
 import Hero from "@/components/hero";
 import Paragraph from "@/components/paragraph";
+import TiltedCard from "@/components/ui/tilted-card";
 
 
 export default function Home() {
@@ -14,6 +15,27 @@ export default function Home() {
 			<Cards />
 			<Paragraph value={paragraph} />
 			<BentoGrid />
+			<div className="p-64">
+				<TiltedCard
+					imageSrc="/images/map.webp"
+					altText=""
+					captionText=""
+					containerHeight="300px"
+					containerWidth="300px"
+					imageHeight="300px"
+					imageWidth="300px"
+					rotateAmplitude={12}
+					scaleOnHover={1.2}
+					showMobileWarning={false}
+					showTooltip={false}
+					displayOverlayContent={true}
+					// overlayContent={
+					// 	<p className="bg-white rounded-full p-2">
+					// 		Coucou les congolais
+					// 	</p>
+					// }
+				/>
+			</div>
 		</main>
     );
 }

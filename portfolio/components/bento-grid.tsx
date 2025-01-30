11471edf7703199bab2729f6d60e5a6cb465b9e9
Bora <borabalos9@gmail.com>
2025-01-30 15:07:37 +0100

    style: Custom bento element with paris map and memoji on top

--- portfolio/components/bento-grid.tsx
+++ portfolio/components/bento-grid.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import BentoElement from "./ui/bento-element";
 
 const BentoGrid = () => {
@@ -7,7 +8,12 @@ const BentoGrid = () => {
                 <BentoElement className="col-span-1 md:col-span-1">Grid 1</BentoElement>
                 <BentoElement className="col-span-1 md:col-span-2">Grid 2</BentoElement>
                 <BentoElement className="col-span-1 md:col-span-2">Grid 3</BentoElement>
-                <BentoElement className="col-span-1 md:col-span-1">Grid 4</BentoElement>
+                <BentoElement className="col-span-1 md:col-span-1 relative">
+                    <Image src="/images/map.webp" alt="Map" width="3000" height="3000" className="absolute z-0 w-full h-full object-cover object-center contrast-[140%] grayscale" />
+                    <div className="absolute inset-0 flex items-center justify-center">
+                        <Image src="/images/memoji.webp" alt="Memoji" width="500" height="500" className="h-16 w-16 rounded-full object-cover border border-white" />
+                    </div>
+                </BentoElement>
             </div>
         </div>
     );
--- portfolio/components/ui/bento-element.tsx
+++ portfolio/components/ui/bento-element.tsx
@@ -8,7 +8,7 @@ const BentoElement: React.FC<BentoElementProps> = ({
     children
 }) => {
     return (
-        <div className={`w-full h-[320px] bg-zinc-100 dark:bg-zinc-900 border border-zinc-300 dark:border-zinc-700 rounded-3xl overflow-hidden ${className}`}>
+        <div className={`relative w-full h-[320px] bg-zinc-100 dark:bg-zinc-900 border border-zinc-300 dark:border-zinc-700 rounded-3xl overflow-hidden ${className}`}>
             {children}
         </div>
     );

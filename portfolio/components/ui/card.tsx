378ad120bb264fd95b3a516b14b0254aaec64d89
Bora <borabalos9@gmail.com>
2025-02-09 13:44:43 +0100

    style: Restyling the stacking cards for experiences

--- portfolio/components/ui/card.tsx
+++ portfolio/components/ui/card.tsx
@@ -30,21 +30,28 @@ const Card: React.FC<CardProps> = ({
     return (
         <div className="h-screen p-4 sm:p-8 md:p-20 flex items-center justify-center sticky top-0">
             
-            <motion.div style={{ scale: cardScale, top: `calc(-10% + ${index * 5}vh)` }} className="relative -top-[10%] w-full max-w-5xl h-auto aspect-video p-2 sm:p-4 bg-[#F4F4F5] dark:bg-zinc-900 border border-zinc-300 dark:border-zinc-700 rounded-xl grid grid-cols-1 lg:grid-cols-2 gap-4">
-                <div className="cols-span-1 flex flex-col justify-between gap-4 order-2 lg:order-1">
-                    <div className="flex flex-col gap-2">
-                        <h2 className="text-xl md:text-3xl">{title}</h2>
-                        <p className="text-sm md:text-base">{description}</p>
+            <motion.div 
+                style={{ scale: cardScale, top: `calc(-5% + ${index * 5}vh)` }} 
+                className="relative -top-[10%] w-full max-w-5xl h-auto p-2 bg-background dark:bg-background border border-zinc-300 dark:border-zinc-700 rounded-3xl flex flex-col gap-4"
+            >
+                <div
+                    className="w-full h-full flex flex-col gap-6 p-6 bg-white dark:bg-zinc-900 rounded-2xl"
+                >
+                    <div className="flex flex-col justify-between gap-4">
+                        <div className="flex flex-col gap-2">
+                            <h2 className="text-xl md:text-3xl">{title}</h2>
+                            <p className="text-sm md:text-base">{description}</p>
+                        </div>
+                        <Link href={link} target="blank">View Project</Link>
                     </div>
-                    <Link href={link} target="blank">View Project</Link>
+                    <Image 
+                        src={src} 
+                        alt=""
+                        width={1000}
+                        height={1000}
+                        className="w-full rounded-2xl pointer-events-none border"    
+                    />
                 </div>
-                <Image 
-                    src={src} 
-                    alt=""
-                    width={500}
-                    height={500}
-                    className="cols-span-1 order-1 lg:order-2 w-full rounded-md pointer-events-none"    
-                />
             </motion.div>
         </div>
     );

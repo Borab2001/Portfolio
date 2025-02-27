5ccf2df76108fc8937fa80a0672c788e092c1dfc
Bora <borabalos9@gmail.com>
2025-02-27 01:29:48 +0100

    style: Passed the paragraph inside the component + wrote the final text + added detail in bento elements

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -6,15 +6,12 @@ import PolaroidStack from "@/components/polaroid-stack";
 // import BentoGrid from "@/components/bento-grid";
 
 
-export default function Home() {
-
-	const paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-	
+export default function Home() {	
     return (
 		<main className="flex flex-col min-h-screen font-[family-name:var(--font-geist-sans)]">
 			<Hero />
 			<Cards />
-			<Paragraph value={paragraph} />
+			<Paragraph />
 			<PolaroidStack />
 			<Experience />
 			{/* <BentoGrid /> */}
--- portfolio/components/bento-grid.tsx
+++ portfolio/components/bento-grid.tsx
@@ -13,9 +13,22 @@ const BentoGrid = () => {
                     </div>
                 </BentoElement>
                 <BentoElement className="col-span-1 md:col-span-2">Grid 2</BentoElement>
-                <BentoElement className="col-span-1 md:col-span-2">Grid 3</BentoElement>
+                <BentoElement className="flex flex-col col-span-1 md:col-span-2">
+                    <p>
+                        10 years of football
+                    </p>
+                    <p>
+                        crafting music on fl studio
+                    </p>
+                    <p>
+                        Food distribution for people in need at Paris City Hall
+                    </p>
+                    <p>
+                        Computer Science Degree   
+                    </p>
+                </BentoElement>
                 <BentoElement className="col-span-1 md:col-span-1 relative">
-                    <Image src="/images/map.webp" alt="Map" width="3000" height="3000" className="absolute z-0 w-full h-full object-cover object-center contrast-[140%] grayscale" />
+                    <Image src="/images/map.webp" alt="Map" width="3000" height="3000" className="opacity-50 absolute z-0 w-full h-full object-cover object-center contrast-[140%] grayscale" />
                     {/* <div className="w-full h-full">
                         <div className="h-full relative flex items-center justify-center">
                             <Image src="/images/memoji.webp" alt="Memoji" width="500" height="500" className="z-10 h-16 w-16 rounded-full object-cover border border-white" />

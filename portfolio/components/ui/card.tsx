179cac9118c3f05e7901d1c4934f2fcae634b542
Bora <borabalos9@gmail.com>
2025-03-02 18:01:26 +0100

    fix: Cards responsive radius

--- portfolio/components/ui/card.tsx
+++ portfolio/components/ui/card.tsx
@@ -32,10 +32,10 @@ const Card: React.FC<CardProps> = ({
             
             <motion.div 
                 style={{ scale: cardScale, top: `calc(-5% + ${index * 5}vh)` }} 
-                className="relative -top-[10%] w-full max-w-4xl h-auto p-2 bg-background dark:bg-background border border-zinc-300 dark:border-zinc-700 rounded-3xl flex flex-col gap-4"
+                className="relative -top-[10%] w-full max-w-4xl h-auto p-2 bg-background dark:bg-background border border-zinc-300 dark:border-zinc-700 rounded-xl sm:rounded-2xl md:rounded-3xl flex flex-col gap-4"
             >
                 <div
-                    className="w-full h-full flex flex-col gap-6 p-4 md:p-6 bg-white dark:bg-zinc-900 rounded-2xl"
+                    className="w-full h-full flex flex-col gap-6 p-4 md:p-6 bg-white dark:bg-zinc-900 rounded-lg sm:rounded-xl md:rounded-2xl"
                 >
                     <div className="flex flex-col justify-between gap-4">
                         <div className="flex flex-col gap-2.5">
@@ -50,7 +50,7 @@ const Card: React.FC<CardProps> = ({
                         alt=""
                         width={1000}
                         height={1000}
-                        className="w-full rounded-xl md:rounded-2xl pointer-events-none border border-zinc-200 dark:border-zinc-800"    
+                        className="w-full rounded-lg sm:rounded-xl md:rounded-2xl pointer-events-none border border-zinc-200 dark:border-zinc-800"    
                     />
                 </div>
             </motion.div>

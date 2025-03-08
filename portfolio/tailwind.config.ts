8ff76136866dd357671ed55ddad9f2b4d5dc3f22
Bora <borabalos9@gmail.com>
2025-03-08 16:05:54 +0100

    clean: Removed useless animations and keyframes in tailwind config

--- portfolio/tailwind.config.ts
+++ portfolio/tailwind.config.ts
@@ -12,19 +12,8 @@ export default {
 				background: "var(--background)",
 				foreground: "var(--foreground)",
 			},
-			animation: {
-				ripple: "ripple var(--duration,2s) ease calc(var(--i, 0)*.2s) infinite",
-			},
-			keyframes: {
-				ripple: {
-					"0%, 100%": {
-						transform: "translate(-50%, -50%) scale(1)",
-					},
-					"50%": {
-						transform: "translate(-50%, -50%) scale(0.9)",
-					},
-				},
-			},
+			animation: {},
+			keyframes: {},
 		},
 	},
   	plugins: [],

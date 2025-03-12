f9ab6dde079625d2bca67f7e65a8f220619966ae
Bora <borabalos9@gmail.com>
2025-03-12 16:01:10 +0100

    fix: Trying transition to fix height snaping on dvh

--- portfolio/components/hero.tsx
+++ portfolio/components/hero.tsx
@@ -4,7 +4,7 @@ import CircularText from "./ui/circular-text";
 
 const Hero = () => {
     return (
-        <div className="min-h-dvh w-full p-4 sm:p-8 md:p-20 flex flex-col">
+        <div className="min-h-dvh w-full p-4 sm:p-8 md:p-20 flex flex-col smooth-height">
             <div className="flex-1 h-full flex flex-col items-center justify-center gap-4 sm:gap-6 md:gap-8">
                 <Image 
                     src="/images/memoji.webp" 

f5aa8946cdf03519bfdcd7564201ae5ecd69cd4e
Bora <borabalos9@gmail.com>
2025-08-30 16:16:21 +0200

    feat: Mapping mockup arrays + personas responsiveness

--- portfolio/components/project.tsx
+++ portfolio/components/project.tsx
@@ -9,22 +9,25 @@ export default function Project({ project }: ProjectProps) {
     return (
         <div className="w-full py-16 border-b border-gray-800 last:border-b-0">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-16">
-                {project.mockupImages.map((image, index) => (
-                    <div key={index} className="w-full aspect-[4/3] md:aspect-[5/5] lg:aspect-[6/5] border border-zinc-700 rounded-lg sm:rounded-xl md:rounded-2xl overflow-hidden">
-                            <Image
-                                src={image}
-                                alt={`${project.title} mockup ${index + 1}`}
-                                width={160}
-                                height={320}
-                                className="w-full h-full object-cover"
-                            />
+                {project.mockupImages.map((group, groupIndex) => (
+                    <div key={groupIndex} className="p-[2vw] flex flex-row justify-center space-x-4 w-full h-auto max-h-[360px] md:h-full md:max-h-none aspect-[4/3] md:aspect-[5/5] lg:aspect-[6/5] bg-[#121212] border border-zinc-700 rounded-lg sm:rounded-xl md:rounded-2xl overflow-hidden">
+                        {group.map((image, imageIndex) => (
+                            <div key={imageIndex} className="">
+                                <Image
+                                    src={image}
+                                    alt={`${project.title} mockup group ${groupIndex + 1} image ${imageIndex + 1}`}
+                                    width={160}
+                                    height={320}
+                                    className="w-full h-full object-contain"
+                                />
+                            </div>
+                        ))}
                     </div>
                 ))}
             </div>
-
-            <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mb-16">
+            <div className="grid grid-cols-1 sm:grid-cols-3 gap-x-4 gap-y-10 mb-16">
                 <div className="col-span-1">
-                    <h2 className="text-2xl font-semibold text-white mb-1">{project.title}</h2>
+                    <h2 className="text-xl md:text-2xl font-semibold text-white mb-1">{project.title}</h2>
                     <p className="text-gray-400 text-md">{project.subtitle}</p>
                 </div>
                 <div className="col-span-2">
@@ -58,26 +61,16 @@ export default function Project({ project }: ProjectProps) {
             </div>
 
             {project.personas && (
-                <div className="grid grid-cols-2 gap-4 justify-items-center">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-items-center">
                     {project.personas.images.map((image, index) => (
-                    // <div key={index} className="w-full aspect-[5/3] border border-zinc-700 rounded-lg sm:rounded-xl md:rounded-2xl overflow-hidden">
-                    //     <Image
-                    //         src={image}
-                    //         alt={`Persona ${index + 1}`}
-                    //         width={400}
-                    //         height={600}
-                    //         className="w-full h-auto object-cover"
-                    //     />
-                    // </div>
-
-                    <Image
-                        key={index}
-                        src={image}
-                        alt={`Persona ${index + 1}`}
-                        width={400}
-                        height={600}
-                        className="w-full aspect-[5/3] border border-zinc-700 rounded-lg sm:rounded-xl md:rounded-2xl overflow-hidden"
-                    />
+                        <Image
+                            key={index}
+                            src={image}
+                            alt={`Persona ${index + 1}`}
+                            width={400}
+                            height={600}
+                            className="w-full aspect-[5/3] border border-zinc-700 rounded-lg sm:rounded-xl md:rounded-2xl overflow-hidden"
+                        />
                     ))}
                 </div>
             )}

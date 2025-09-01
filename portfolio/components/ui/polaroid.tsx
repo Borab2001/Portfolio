3cd0a51039ca0258eb614758b779dcf158f9064f
Bora <borabalos9@gmail.com>
2025-09-01 15:20:36 +0200

    feat: Custom polaroid component

---
+++ portfolio/components/ui/polaroid.tsx
@@ -0,0 +1,52 @@
+import Image from 'next/image';
+import { Kalam } from "next/font/google";
+
+interface PolaroidPhotoProps {
+  image: string;
+  alt: string;
+  imageTitle: string;
+  date: string;
+  position: 'left' | 'right';
+}
+
+const kalamFont = Kalam({
+    subsets: ["latin"],
+    weight: "400",
+});
+
+export default function PolaroidPhoto({ image, alt, imageTitle, date, position }: PolaroidPhotoProps) {
+    return (
+        <div className={`relative ${position === 'left' ? 'mr-8' : 'ml-8'}`}>
+            <Image 
+                src="/images/polaroid/pin.svg"
+                alt="Pin"
+                className={`
+                    absolute -top-3 left-1/2 transform z-20
+                    ${position === 'left' ? 'scale-x-[1] -translate-x-[100%]' : 'scale-x-[-1] -translate-x-[0%]'}
+                `}
+                width={64}
+                height={64}
+            />
+
+            <div
+                className={`
+                    w-[320px] h-[400px] overflow-hidden bg-surface p-4 flex flex-col items-center justify-start pointer-events-none
+                    ${position === 'left' ? 'rotate-[-8deg]' : 'rotate-[6deg]'}
+                `}
+            >
+                <Image
+                    width={288}
+                    height={288}
+                    src={image}
+                    alt={alt}
+                    className="w-[288px] h-[288px] object-cover select-none pointer-events-none"
+                    loading="lazy"
+                />
+                <div className={`w-full pt-4 flex flex-col gap-2 select-none pointer-events-none ${kalamFont.className}`}>
+                    <h2 className="text-2xl text-surface-text font-semibold">{imageTitle}</h2>
+                    <p className="text-base text-surface-muted">{date}</p>
+                </div>
+            </div>
+        </div>
+    );
+}

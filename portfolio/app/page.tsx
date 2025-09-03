825d27fda2dca6ebf7f9718ba90d964a770174ba
Bora <borabalos9@gmail.com>
2025-09-03 18:38:50 +0200

    data: Started writing some of the timeline content and adjusted padding for paragraph

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/hero";
 import Cards from "@/components/cards";
-// import Paragraph from "@/components/paragraph";
+import Paragraph from "@/components/paragraph";
 // import PolaroidStack from "@/components/polaroid-stack";
 import Experience from "@/components/experience";
 import Footer from "@/components/footer";
@@ -14,7 +14,7 @@ export default function Home() {
 			id: '1',
 			year: 2019,
 			title: 'ISEP - Engineering School',
-			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+			description: 'After obtaining my french baccalaureat abroad, I decided to move in France to pursue my higher education at ISEP, an engineering school based in Paris. In 2019 I started my preparatory classes there.',
 			image: '/images/polaroid/isep.webp',
 			alt: 'ISEP Campus',
 			imageTitle: 'ISEP Campus',
@@ -25,7 +25,7 @@ export default function Home() {
 			id: '2',
 			year: 2023,
 			title: 'University of Sydney',
-			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+			description: 'Spending a semester in Sydney was one of the most incredible experiences of my life. I had the chance to learn from insanely talented designers and engineers from companies like Canva, Bloomberg, and Spotify—people who truly opened my eyes to new ways of thinking about design and technology.',
 			image: '/images/polaroid/usyd.webp',
 			alt: 'Sydney Uni Quadrangle',
 			imageTitle: 'Sydney Uni Quadrangle',
@@ -35,7 +35,7 @@ export default function Home() {
 		{
 			id: '3',
 			year: 2023,
-			title: 'Experience Name 1',
+			title: 'Sydney #1',
 			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
 			image: '/images/polaroid/opera_night.webp',
 			alt: 'Sydney Opera House view by night',
@@ -46,8 +46,8 @@ export default function Home() {
 		{
 			id: '4',
 			year: 2023,
-			title: 'Experience Name 2',
-			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+			title: 'Sydney #2',
+			description: 'But beyond the academics, what really stuck with me was the city itself. Sydney is just… different. The energy, the people, the diversity—it all made me feel at home in a way I didn’t expect. ',
 			image: '/images/polaroid/wc_final.webp',
 			alt: 'Women\'s World Cup Final in Sydney at ANZ Stadium',
 			imageTitle: 'Women\'s World Cup Final',
@@ -57,23 +57,34 @@ export default function Home() {
 		{
 			id: '5',
 			year: 2023,
-			title: 'Experience Name 3',
-			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+			title: 'Sydney #3',
+			description: 'I fell in love with the culture, the spontaneity of everyday life, the endless food spots where you could taste the whole world in one city, and of course, those golden-hour walks by the beaches. It was the kind of place where every day felt like an adventure, and leaving it only made me more sure that I want to come back. Sydney felt like home, and I can’t wait to go back.',
 			image: '/images/polaroid/bronte_beach.webp',
 			alt: 'Sunrise in Bronte Beach in Sydney',
 			imageTitle: 'Bronte Beach',
 			date: 'Dec 2023 at 5:40AM',
 			position: 'left'
+		},
+		{
+			id: '6',
+			year: 2025,
+			title: 'Graduation',
+			description: 'In 2025, I earned my Master’s degree in Computer Science at ISEP, where I spent years honing my technical skills and diving deep into software engineering and design.',
+			image: '/images/polaroid/isep.webp',
+			alt: 'ISEP Graduation',
+			imageTitle: 'ISEP Graduation',
+			date: 'May 2025 at 10:00AM',
+			position: 'right'
 		}
 	];
 
     return (
 		<main className="backdrop-blur-lg flex flex-col min-h-screen font-[family-name:var(--font-geist-sans)]">
 			<Hero />
-			<Cards />
-			{/* <Paragraph /> */}
-			{/* <PolaroidStack /> */}
+			<Paragraph />
 			<Timeline experiences={timelineData} />
+			<Cards />
+			{/* <PolaroidStack /> */}
 			<Experience />
 			<Footer />
 		</main>
--- portfolio/components/paragraph.tsx
+++ portfolio/components/paragraph.tsx
@@ -19,7 +19,7 @@ const Paragraph: React.FC = () => {
     const words = text.split(/(\s+|<br \/>)/);
 
     return (
-        <div ref={parentRef} className="relative h-auto mt-48">
+        <div ref={parentRef} className="relative h-auto my-48">
             <div className="w-full max-w-6xl mx-auto flex items-center justify-center h-auto px-4 pt-4 sm:px-10 md:px-12 lg:px-24 lg:pt-24">
                 <p className="flex flex-wrap justify-start gap-y-2 gap-x-1 md:gap-y-6 md:gap-x-1 text-xl md:text-3xl tracking-tight text-foreground">
                     {words.map((word, index) => {

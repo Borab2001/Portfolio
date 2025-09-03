2e9f4f5ade4baf63ab72c6ed81d5d91a617d2848
Bora <borabalos9@gmail.com>
2025-09-03 04:53:33 +0200

    style: Max width for containers

--- portfolio/app/projects/page.tsx
+++ portfolio/app/projects/page.tsx
@@ -78,7 +78,7 @@ export default function Projects() {
 
     return (
 		<main className="backdrop-blur-lg flex flex-col min-h-screen font-[family-name:var(--font-geist-sans)]">
-            <div className="min-h-dvh w-full pt-4 px-4 sm:pt-8 sm:px-8 flex flex-col smooth-height">
+            <div className="min-h-dvh max-w-[1600px] mx-auto w-full pt-4 px-4 sm:pt-8 sm:px-8 flex flex-col smooth-height">
                 <div className="flex-1 h-full flex flex-col items-center justify-center gap-4 sm:gap-6 md:gap-8 pt-[24vh] pb-16">
                     <h1 className="text-5xl sm:text-7xl md:text-8xl font-medium text-primary text-center">
                         Projects
@@ -102,7 +102,7 @@ export default function Projects() {
                     ))}
                 </div>
             </div>
-            <div className='flex flex-col pt-4 px-4 sm:pt-8 sm:px-8 gap-8 sm:gap-10 md:gap-12'>
+            <div className='max-w-[1600px] mx-auto flex flex-col pt-4 px-4 sm:pt-8 sm:px-8 gap-8 sm:gap-10 md:gap-12'>
                 <h2 className="text-4xl md:text-5xl lg:text-6xl font-normal text-primary text-center">
                     Some Mockups
                 </h2>

992a650a1cb10c9c8da3a8998ea931bb731649bb
Bora <borabalos9@gmail.com>
2025-10-16 16:48:12 +0200

    style: Removed bg color in projects section

--- portfolio/components/project-section.tsx
+++ portfolio/components/project-section.tsx
@@ -5,6 +5,7 @@ import { useRef } from "react";
 import ProjectCard from "./ui/project-card";
 import { Project } from "@/types/project";
 
+
 interface ProjectSectionProps {
     projects: Project[];
 }
@@ -18,14 +19,14 @@ const ProjectSection = ({ projects }: ProjectSectionProps) => {
     const x = useTransform(scrollYProgress, [0, 1], ["1%", "-95%"]);
 
     return (
-        <section ref={targetRef} className="relative h-[300vh] bg-neutral-900">
-        <div className="sticky top-0 flex h-screen items-center overflow-hidden">
-            <motion.div style={{ x }} className="flex gap-4">
-                {projects.map((project) => {
-                    return <ProjectCard key={project.id} project={project} />;
-                })}
-            </motion.div>
-        </div>
+        <section ref={targetRef} className="relative h-[300vh]">
+            <div className="sticky top-0 flex h-screen items-center overflow-hidden">
+                <motion.div style={{ x }} className="flex gap-4">
+                    {projects.map((project) => {
+                        return <ProjectCard key={project.id} project={project} />;
+                    })}
+                </motion.div>
+            </div>
         </section>
     );
 };
